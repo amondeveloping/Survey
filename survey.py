0f65adc7067d3e5648f28ed48e0c70a320f63f45
Adrian Montejo <97710906+bullybred@users.noreply.github.com>
2022-07-11 19:21:25 -0400

    update

---
+++ survey.py
@@ -0,0 +1,23 @@
+name = input('What is your name? ')
+print()
+print('Hi ' + name)
+print()
+
+age = input('How old are you? ')
+print()
+print('Just a spring chicken! ')
+print()
+
+color = input('What is your favourite color? ')
+print()
+print(color + ' is my favourite color too! ')
+print()
+
+model = input('What kind of car do you drive? ')
+print()
+print(model + ' is a nice vehicle! ')
+print()
+
+print('Thank you for your time ' + name)
+
+
